ab7648bd50869b7eb6f3bfeae0653bd844a7dc30
Darick <darickmcbride@me.com>
2023-11-11 11:55:59 -0800

    installed mui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
+import type { Metadata } from 'next'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+export const metadata: Metadata = {
+  title: 'Darick\'s Trivia App',
+  description: 'Trivia app created by Darick McBride',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  )
+}
