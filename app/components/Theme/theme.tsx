c1a0941aa9bea3a9e5c15cebdaddcd9096d29d9d
Darick <darickmcbride@me.com>
2023-11-12 16:57:14 -0800

    init

---
+++ app/components/Theme/theme.tsx
@@ -0,0 +1,19 @@
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  // palette: {
+  //   mode: "dark",
+  //   primary: {
+  //     main: "#157EBF",
+  //   },
+  //   secondary: {
+  //     main: "#05C7F2",
+  //   },
+  //   background: {
+  //     default: "#121212",
+  //   },
+  //},
+});
+
+export default theme;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import type { Metadata } from "next";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import ThemeRegistry from "./components/Theme/ThemeRegistry";
 
 export const metadata: Metadata = {
-  title: 'Darick\'s Trivia App',
-  description: 'Trivia app created by Darick McBride',
-}
+  title: "Darick's Trivia App",
+  description: "Trivia app created by Darick McBride",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>
+        <ThemeRegistry options={{ key: "mui" }}>{children}</ThemeRegistry>
+      </body>
     </html>
-  )
+  );
 }
