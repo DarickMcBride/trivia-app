ff0bafc4afbff2a751dcc6f88c060bc2c1419026
Darick <darickmcbride@me.com>
2023-11-13 16:27:38 -0800

    templated about page

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Welcome to my trivia app! I am a passionate developers who love creating
+        fun and engaging experiences for my users.
+      </p>
+      <p>
+        My goal is to provide a platform for people to test their knowledge and
+        have fun while doing it. I hope you enjoy using my app as much as I
+        enjoyed building it!
+      </p>
+      <br />
+      <p>Thank you,</p>
+      <p>Darick McBride</p>
+    </div>
+  );
+};
+
+export default AboutPage;
