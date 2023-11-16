f732c773c8b9a66dfc741ba8942e2adadfd2ef40
Darick <darickmcbride@me.com>
2023-11-15 19:43:54 -0800

    commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,7 @@
 export async function getQuestions() {
   const res = await fetch(
-    "https://opentdb.com/api.php?amount=50&type=multiple&encode=base64"
+    "https://opentdb.com/api.php?amount=50&type=multiple&encode=base64",
+    { method: "GET" }
   );
 
   if (!res.ok) {
@@ -9,5 +10,3 @@ export async function getQuestions() {
 
   return res.json();
 }
-
-
