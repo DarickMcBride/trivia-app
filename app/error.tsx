d61d3b5a90dc09eda1e923145c9f2219ccedd04f
Darick <darickmcbride@me.com>
2023-11-15 21:26:22 -0800

    commit

--- app/error.tsx
+++ app/error.tsx
@@ -15,7 +15,7 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
+    <div style={{ marginLeft: 10 }}>
       <h2>Something went wrong!</h2>
       <p>{error.message}</p>
       <button
