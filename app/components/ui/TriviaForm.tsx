55174c2a3a0695d241d68503a154f2137ff2e612
Darick <darickmcbride@me.com>
2023-11-21 18:23:17 -0800

    commit

--- app/components/ui/TriviaForm.tsx
+++ app/components/ui/TriviaForm.tsx
@@ -7,7 +7,6 @@ import { FormButton } from "./FormButton";
 import { useFormState } from "react-dom";
 import { DataContext } from "@/app/providers";
 import { submitAnswer } from "@/app/lib/actions";
-import { getCookies } from "cookies-next";
 
 const initialState = {
   message: "",
@@ -18,10 +17,6 @@ const TriviaForm = () => {
   const [questions, setQuestions] = useContext(DataContext);
 
   const [state, formAction] = useFormState(submitAnswer, initialState);
-
-  //get user id from cookie
-  const userID = getCookies();
-
 
   const handleSubmit = (formData: any) => {
     formAction(formData);
@@ -64,8 +59,8 @@ const TriviaForm = () => {
           type="hidden"
           name="question-id"
           value={questions[0]?.id !== undefined ? questions[0]?.id : -1}
-        />{" "}
-        <input type="hidden" name="user-id" value={userID} />
+        />
+
         {!submitted && (
           <>
             <Suspense fallback={<p>Loading answers...</p>}>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,31 +8,27 @@ export async function submitAnswer(prevState: any, formData: FormData) {
   try {
     const id = formData.get("question-id");
     const answer = formData.get("answer-select");
-    const userID = formData.get("user-id");
 
-    if (!userID) {
-      throw new Error("User ID not found");
-    }
 
-    const res = await getQuestions(userID?.toString());
-    const data = res.results;
+  //   const res = await getQuestions(userID?.toString());
+  //   const data = res.results;
 
-    const question = data[id?.toString() || -1];
+  //   const question = data[id?.toString() || -1];
 
-    if (!question) {
-      return;
-    }
+  //   if (!question) {
+  //     return;
+  //   }
 
-    const correctAnswer = question.correct_answer;
+  //   const correctAnswer = question.correct_answer;
 
-    //check if answer is correct
-    if (answer === correctAnswer) {
-      return { message: "Correct!" };
-    } else {
-      return { message: "Incorrect!" };
-    }
-  } catch (error: any) {
-    console.error(error);
-    return { message: error.message };
-  }
+  //   //check if answer is correct
+  //   if (answer === correctAnswer) {
+  //     return { message: "Correct!" };
+  //   } else {
+  //     return { message: "Incorrect!" };
+  //   }
+  // } catch (error: any) {
+  //   console.error(error);
+  //   return { message: error.message };
+  // }
 }
